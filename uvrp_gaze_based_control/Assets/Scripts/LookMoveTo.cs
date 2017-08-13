345eef2055518b942073118fbfa19180d60676f1
Pablo Perotti <pablo@blackpixel.com>
2017-08-13 00:31:51 -0300

    feat: Add game that kills zombie.

---
+++ uvrp_gaze_based_control/Assets/Scripts/LookMoveTo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookMoveTo : MonoBehaviour {
+
+	public GameObject ground;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform camera = Camera.main.transform;
+		Ray ray;
+		RaycastHit[] hits;
+		GameObject hitObject;
+
+		ray = new Ray (camera.position, camera.rotation * Vector3.forward);
+
+		hits = Physics.RaycastAll (ray);
+		for (int i = 0; i < hits.Length; i++) {
+			RaycastHit hit = hits [i];
+			hitObject = hit.collider.gameObject;
+			if (hitObject == ground) {
+				Debug.Log ("Hit (x,y,z): " + hit.point.ToString("F2"));
+				transform.position = hit.point;
+			}
+		}
+
+		/*if (Physics.Raycast (ray, out hit)) {
+			hitObject = hit.collider.gameObject;
+			if (hitObject == ground) {
+				Debug.Log ("Hit (x,y,z): " + hit.point.ToString("F2"));
+				transform.position = hit.point;
+			}
+		}*/
+	}
+}
