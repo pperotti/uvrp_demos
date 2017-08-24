6218d04a49f30c99474a97f734761ef413947fe4
Pablo Perotti <pablo@blackpixel.com>
2017-08-24 11:48:17 -0300

    feat: Add physics project to host its demo's scenes.

---
+++ uvrp_physics/Assets/Ballgame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballgame : MonoBehaviour {
+
+	public GameObject ball;
+	public float startHeight = 10f;
+	public float fireInterval = 5f;
+
+	private float nextBallTime = 0.0f;
+	private GameObject activeBall;
+	private Transform head;
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		head = Camera.main.transform;
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextBallTime) {
+			nextBallTime = Time.time + fireInterval;
+			audio.Play ();
+			Vector3 position = new Vector3( head.position.x, startHeight, head.position.z + 0.2f );
+			activeBall = Instantiate( ball, position, Quaternion.identity ) as GameObject;
+		}	
+	}
+}
---
+++ uvrp_physics/Assets/TriggerSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour {
+
+	public AudioClip hitSound;
+
+	AudioSource aa;
+
+	// Use this for initialization
+	void Start () {
+		aa = GetComponent < AudioSource> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		aa.Play ();
+	}
+}
